1d8699a57cbcb2b6d768666eac69f5ed0ed7f7f7
ImDH <eastring2343@naver.com>
2025-11-25 10:57:47 +0900

    feat: prob04.cpp update
    Implement floor and ceiling functions to find values in an array.

---
+++ Assignment5/prob04.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 1000;
+
+int ignite[MAX];
+
+int floor(int end, int K) {
+	if (end < 0) { return -1; }
+
+	if (ignite[end] > K) return floor(end - 1, K);
+	if (ignite[end] < K) return ignite[end];
+}
+
+int ceiling(int start, int end, int K) {
+	if (start > end) { return -1; }
+
+	if (ignite[start] < K) return ceiling(start + 1, end, K);
+	if (ignite[start] >= K) return ignite[start];
+}
+
+int main(void) {
+	int N, K;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> ignite[i];
+	}
+	cin >> K;
+
+	cout << endl << floor(N - 1, K) << endl << ceiling(0, N - 1, K);
+	return 0;
+}
